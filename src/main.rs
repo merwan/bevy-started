f3cd0cd0ce972a47bf7330130c327880471368c2
Merouane Atig <merwan@patem.net>
2024-06-30 23:13:54 +0200

    Track time

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,14 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
 fn main() {
     App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
-}
-
-fn hello_world() {
-    println!("Hello World!");
 }
 
 #[derive(Component)]
@@ -23,15 +20,20 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Joe Aze".to_string())));
     commands.spawn((Person, Name("Jennie Qus".to_string())));
     commands.spawn((Person, Name("Billie Wox".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
     }
 }
 
